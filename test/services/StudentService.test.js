bdc17ad8c28c7c50ee816694d55919c77ac391a6
Ajelandro19 <almon190699@gmail.com>
2022-04-29 20:09:53 -0500

    Crear requerimiento 2 para Studentservice

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -6,4 +6,9 @@ describe("Tests para Student Services.", ()=>{
         const lista=studentservice.getlistadealumnos(alumnos)
         expect(lista.length).toBe(2)
     })
+    test("Requerimiento 2: Obtener lista de estudiantes con certificado", ()=>{
+        const alumnos= [{name: "Ajelandro", id:"12121asd123", haveCertification: true },{name: "Alex", id:"121dsad32132", haveCertification: false }]
+        const lista=studentservice.getalumnosconcertificado(alumnos)
+        expect(lista.length).toBe(1)
+    })
 })
